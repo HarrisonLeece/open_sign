58fb7899da24b39c5dd6175dec6d23399ba100d3
harrisonLeece <harryleecemail@gmail.com>
2021-10-01 13:29:33 -0700

    weather data loads

--- js/weather_api.js
+++ js/weather_api.js
@@ -1,23 +1,47 @@
-function get_weather(url) {
-  return fetch(url)
+async function get_weather(url) {
+  let response = await fetch(url)
+  let data = await response.json()
+  console.log(data)
+  return data
 }
-var video = document.getElementById('weather_bar');
 
-//Since the data only updates every 3 hours, only check for update every 10 mins
-let weather_interval = 10000;
-//let interval = 14400000
-let json_data = {};
+function change_weather_html(data) {
+  var video = document.getElementById('weather_bar');
+
+  //All this date comes out in metric;;; Temperature in Kelvin
+  let temp = data.main.temp
+  let temp_min = data.main.temp_min
+  let temp_max = data.main.temp_max
+  let humidity = data.main.humidity
+
+  let icon = data.weather.icon
+
+}
+
+
+//Since the data only updates every 3 hours, only check for update every min
+let weather_interval = 5000;
+//So the reason I run the code up here before the set interval is Because
+//extracted_data is null until the code has been executed once
+//This is just because of race conditions, since get_weather is non-blocking
+//This is a naive solution to allow updating of weather in the first minuite
+//after the server starts
+//It would be solved trivially with a sleep function, a la python
+let extracted_data = {}
+let json_data = get_weather("./js/json/current_weather.json")
+  .then((json_data) => extracted_data=json_data)
+
+console.log(extracted_data);
+let temperature = extracted_data.main.temp
+
+console.log(temperature)
 setInterval(function() {
     //Load JSON file from a child directory (in this case ./js/json/current_weather.json)
-    json_data = get_weather("./js/json/current_weather.json");
+    json_data = get_weather("./js/json/current_weather.json")
+      .then((json_data) => extracted_data=json_data)
+    console.log(extracted_data)
+    temperature = extracted_data.main.temp
+    console.log(temperature)
 
-    json_data.then(response => response.json())
-     .then(data => {
-          console.log(data);
-     })
-     .catch(error => {
-          console.log('Error in fetch')
-     });
-     console.log(json_data);
 
 }, weather_interval);
